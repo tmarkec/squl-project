8ecf13a1704a36b551e8e56098d108b39e5fdc6f
tmarkec <tmarkec@gmail.com>
2023-02-15 16:30:41 +0000

    add delete functionality

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,21 +2,21 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    # schema for category model
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship(
         "Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        # __Repr__ to represent itself in the form of a string
+        # __repr__ to represent itself in the form of a string
         return self.category_name
 
 
 class Task(db.Model):
-    # schema for task model
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
-    task_name = db.Column(db.String(40), unique=True, nullable=False)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
@@ -24,7 +24,7 @@ class Task(db.Model):
         "category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        # __Repr__ to represent itself in the form of a string
+        # __repr__ to represent itself in the form of a string
         return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
